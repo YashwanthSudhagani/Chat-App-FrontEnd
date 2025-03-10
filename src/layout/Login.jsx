346341f410a9ee7befa2a36362980c5ea49e9ea0
YashwanthSudhagani <sudaganiyashwanth@gmail.com>
2025-03-10 12:18:44 +0530

    Updated chat app with latest changes

--- src/layout/Login.jsx
+++ src/layout/Login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
     setError("");
   
     try {
-      const response = await fetch(`${chatURL}/api/auths/login`, {
+      const response = await fetch(`${chatURL}/auths/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
